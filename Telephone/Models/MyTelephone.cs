3c2c8ea5b36ca32b59efb7308c5e4fd2acbecccb
Abbas Asadzada <tu839nxdv@aztu.local>
2022-04-08 16:25:38 +0400

    Modified

--- Telephone/Models/MyTelephone.cs
+++ Telephone/Models/MyTelephone.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Text;
 using Telephone.Interface;
+using System.Threading;
 namespace Telephone.Models
 {
     class MyTelephone : IContact
@@ -14,11 +15,20 @@ namespace Telephone.Models
 
         public void Call(string name)
         {
-             
+            bool isTalking = false;
+            Person person = SearchbyName(name);
+            if (person != null)
+            {
+                Console.WriteLine("Calling");
+                Thread.Sleep(10000);
+                DateTime startTimne = DateTime.UtcNow;
+                int startingTime = startTimne.Millisecond;
+                
+            }
         }
 
         public void CallByNumber(int number)
-        {
+        {   
              
         }
 
@@ -26,5 +36,25 @@ namespace Telephone.Models
         {
             MyContact.Remove(person);
         }
+        public void ShowAll() 
+        {
+            foreach (var item in MyContact)
+            {
+                Console.WriteLine($@"Name-{item.FullName}
+Number-{item.Number}");
+            }
+        }
+        public Person SearchbyName(string name)
+        {
+            foreach (var item in MyContact)
+            {
+                if(item.FullName == name)
+                {
+                    return item;
+                }
+            }
+            return null;
+        }
+
     }
 }
--- Telephone/Models/Person.cs
+++ Telephone/Models/Person.cs
@@ -8,14 +8,16 @@ namespace Telephone.Interface
     {
         public string FullName { get; set; }
         public string Number{ get; set; }
+        public bool IsAviable { get; set; }
+        public int Balance { get; set; }
         public enum Tarif
         {
             GencOl, Serbest, Sade
         }
-        public Person(string FullName  ,int number , Tarif t) 
+        public Person(string FullName  ,int number,int balance , Tarif t) 
         {
             FullName = name;
-            
+            Balance = balance;
             Number = number;
         }
 
